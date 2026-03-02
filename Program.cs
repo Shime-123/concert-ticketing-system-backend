282c3cae5ba25bb9a4ceab390c891573229cf41d
shimelis <shimelisgetachew59@gmail.com>
2026-03-03 02:29:26 +0300

    Complete backend: Stripe webhooks, PDF generation, and QR codes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Concert_Backend.Data;
 using Concert_Backend.Services;
 using Microsoft.EntityFrameworkCore;
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
